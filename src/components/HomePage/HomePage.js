eee30d5dd568333434f13ca2c2ee0e94a76a26f0
pbullion <pbullion@gmail.com>
2019-07-15 12:48:41 -0500

    stuff

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
-import { Container, Row, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { withRouter } from "react-router-dom";
 import "./HomePage.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import { ProductCard } from "react-ui-cards";
 
 class HomePage extends Component {
-  state = {
-  };
+  state = {};
 
-
-  componentWillMount() {
-  }
+  componentWillMount() {}
 
   render() {
     return (
@@ -25,8 +25,40 @@ class HomePage extends Component {
         <Row>
           <h1>GolfaPOOLooza</h1>
         </Row>
+        <Carousel
+          showThumbs={false}
+          showStatus={false}
+          className="presentation-mode"
+          infiniteLoop={true}
+          autoPlay={true}
+          interval={8000}
+        >
+          <div className="my-slide">
+            <img src="https://golfadvisor.brightspotcdn.com/dims4/default/2cfa350/2147483647/strip/true/crop/8412x5024+0+300/resize/1440x860!/format/jpg/quality/90/?url=https%3A%2F%2Fgolfadvisor.brightspotcdn.com%2Ff9%2Ff1%2Ff23a1aeca45b7fc9992a21e32131%2Fp.php" />
+            <div className="slide-text">
+              <h1>Create a group for The Open Championship</h1>
+            </div>
+          </div>
+          <div className="my-slide">
+            <img src="https://miro.medium.com/max/700/1*TG7BbRi_xb81IfWDiCuvEQ.jpeg" />
+            <div className="slide-text">
+              <h1>Presentation mode</h1>
+            </div>
+          </div>
+        </Carousel>
         <Row>
-          <h2>Create your own Fantasy Golf Pool</h2>
+          <ProductCard
+            photos={[
+              "https://i.imgur.com/jRVDeI8.jpg",
+              "https://i.imgur.com/raPe27t.jpg",
+              "https://i.imgur.com/IpEsYSH.jpg"
+            ]}
+            productName="The Open Championship"
+            description="Donec lectus nulla, molestie aliquam nisl vitae, tempor placerat magna. Morbi dignissim in felis vel aliquet."
+            buttonText="Leaderboard"
+            rating={3}
+            url="https://github.com/nukeop"
+          />
         </Row>
       </Container>
     );
