02bc8144b6520f330d3b71fa3f7e898b48f660e9
pbullion <pbullion@gmail.com>
2019-07-19 11:36:50 -0500

    updates

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -116,7 +116,7 @@ class Leaderboard extends Component {
     return (
       <Container fluid>
         <h1>{this.state.tournamentName}</h1>
-        <h2>UPDATED AT: 4:00 pm central</h2>
+        <h2>UPDATED AT: 11:30 am central</h2>
         <table className="table">
           <thead>
             <tr>
